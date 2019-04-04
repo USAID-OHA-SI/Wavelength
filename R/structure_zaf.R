d7c9ceefd4db2c0930bf21a42cd3439200b2a31e
Aaron Chafetz <achafetz@usaid.gov>
2019-04-04 16:55:25 -0400

    add fcn to import ZAF partner data

---
+++ R/structure_zaf.R
@@ -0,0 +1,78 @@
+#' Import and Structure South Africa
+#'
+#' @description This function will be used to take the daily/weekly SA
+#' dataset and structure in a uniform way to be tidy and align with other
+#' partners/OU datasets. Only selected indicators will be imported and transformed
+#' based on OHA's needs. The data is sourced from South Africa's partner's monthly
+#' reporting template files.
+#'
+#' @param filepath filepath to the output file
+#' @param folderpath_output if a txt output is desired, provide the full path to the folder
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' #structure output for ZAF
+#'   path <- "~/WeeklyData/Raw/ZAF_partner_x.xlsx"
+#'   structure_cod(path)
+#' #structure output for ZAF & export to txt file
+#'   structure_cod(path, "~/WeeklyData/Output") }
+
+structure_zaf <- function(filepath, folderpath_output = NULL){
+
+  #import
+  sheets <- c("HTS_TST_POS", "TX_NEW", "TX_NEW_SAMEDAY", "cLTFU", "uLTFU", "TX_CURR_28") %>% purrr::set_names()
+
+  df <- purrr::map_dfr(.x = sheets,
+                       .f = ~ readxl::read_excel(filepath, sheet = .x, col_types = "text"))
+  #check structure
+  #TODO add assert check to make sure structure stays the same
+
+  #reshape long
+    df <- df %>%
+      tidyr::gather(date, val, -FundingAgency:-Facility, na.rm = TRUE) %>%
+      dplyr::mutate(val = as.double(val)) %>%
+      dplyr::filter(val != 0)
+
+  #fix issues with IM and excel date that end with decimal
+    df <- dplyr::mutate_at(df, dplyr::vars(MechanismID, date), ~ as.integer(.) %>% as.character())
+
+  #rename geography for uniformity & rename lower
+    df <- df %>%
+      dplyr::rename(snu1 = Province,
+                    pnsu = District,
+                    community = `Sub-district`) %>%
+      dplyr::select(-`Siyenza Sites`, -TIER) %>%
+      dplyr::rename_all(~tolower(.))
+
+  #adjust from Excel date format
+    df <- df %>%
+      dplyr::mutate(date = lubridate::as_date(as.integer(date), origin = "1899-12-30"),
+                    month = lubridate::month(date, label = TRUE, abbr = FALSE),
+                    fy = lubridate::quarter(date, with_year = TRUE, fiscal_start = 10) %>% stringr::str_sub(., 1, 4)) %>%
+      dplyr::select(-val, dplyr::everything())
+
+  #add disaggregate
+    df <- df %>%
+      tibble::add_column(disaggregate = "Total Numerator", .after = "indicator")
+
+  #add operatingunit
+    df <- df %>%
+      dplyr::mutate(operatingunit = "South Africa") %>%
+      dplyr::select(operatingunit, dplyr::everything())
+
+  #add reporting frequency
+    df <- tibble::add_column(df, reporting_freq = as.character(NA),
+                             after = "facility") %>%
+      dplyr::mutate(reporting_freq = dplyr::case_when(indicator %in% c("HTS_TST_POS", "TX_NEW", "TX_NEW_SAMEDAY") ~ "Daily",
+                                                      indicator %in% c("cLTFU", "uLTFU") ~ "Weekly",
+                                                      indicator %in% c("TX_CURR_28") ~ "Bi-Weekly"))
+    #TODO arrange variable order
+
+  #export
+    if(!is.null(folderpath_output))
+      readr::write_tsv(df, file.path(folderpath_output, paste0("COD_HFD_", format(Sys.Date(),"%Y%m%d"), ".txt")), na = "")
+
+    return(df)
+}

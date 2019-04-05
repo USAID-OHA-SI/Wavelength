a836c73a2b630e58fd5a9d0087ca0c174e38bc97
Aaron Chafetz <achafetz@usaid.gov>
2019-04-05 12:04:49 -0400

    add function for exporting txt file

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,7 @@
 # Wavelength 0.0.0.9000
 
+* Add function for exporting as a .txt, `export_hfd()`, including adding in an ISO code mapping table for naming `data(iso_map)`
+* Added function to name operatingunits, `add_ou()` since this code was replicated in each dataset
 * Added an indicator mapping for Uganda, `data(ind_map_uga)`, with full list of collected indicators and ones that we are currently collecting for OHA, breaking out disaggs.
 * Added function to import Democratic Republic of the Congo partner files, `structure_cod()`
 * Added age mapping for age bands, `data(age_map)` and `apply_agebands()`
---
+++ R/export_hfd.R
@@ -0,0 +1,33 @@
+#' Export High Frequency Data
+#'
+#' @param df structured High Frequency Data Frame
+#' @param folderpath_output provide the full path to the folder for saving
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'  #write output
+#'    export_hfd(df_tza, "TZA", "~/WeeklyData") }
+
+export_hfd <- function(df, folderpath_output = NULL){
+
+  if(!is.null(folderpath_output)){
+
+    #get ISO code for file nameing
+      ou <- unique(df$operatingunit)
+      iso_ou <- dplyr::filter(iso_map, operatingunit == ou) %>%
+        dplyr::pull(iso_ou)
+
+    #get date for file naming
+      date <- format(Sys.Date(),"%Y%m%d")
+
+    #compile file name
+      filename <- paste0("HFD_", iso_ou, "_", date, ".txt")
+
+    #export data
+      readr::write_tsv(df, file.path(folderpath_output, filename), na = "")
+  }
+}
+
+

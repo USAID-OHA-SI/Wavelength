407ac3abf0749b81d3e7b7e0d8d70575136cc14d
Aaron Chafetz <achafetz@usaid.gov>
2019-04-24 09:03:38 -0400

    add visual

---
+++ R/identify_headers_cmr.R
@@ -0,0 +1,45 @@
+#' Create un-umerged, unique header row for Cameroon
+#'
+#' @param filepath full filepath to the country file
+#' @param sheetname tab name
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' #get new header row
+#'   path <- "~/WeeklyData/Cameroon_data.xlsx"
+#'   headers <- identify_headers_cameroon(path, "TX_NEW")
+#' #apply new headers to the import
+#'  df_hti <- readxl::read_excel(path, sheet = "TX_NEW", skip = 1, col_names = headers) }
+
+identify_headers_cmr <- function(filepath, sheetname){
+
+  #first row of headers
+  r1 <- readxl::read_excel(filepath, sheet = sheetname, skip = 3, .name_repair = "minimal") %>%
+    names()
+  #second row of headers
+  r2 <- readxl::read_excel(filepath, sheet = sheetname, skip = 4, .name_repair = "minimal") %>%
+    names()
+  #third row of headers
+  r3 <- readxl::read_excel(filepath, sheet = sheetname, skip = 3, .name_repair = "minimal") %>%
+    names()
+
+  #align vector lengths to combine
+  n <- max(length(r1), length(r2), length(r3))
+  length(r1) <- n
+  length(r2) <- n
+  length(r3) <- n
+
+  #combine, replacing "" with NA & filling down (merged headers) before concatenating
+  headers <-
+    tibble::tibble(r1, r2, r3) %>%
+    dplyr::mutate_all(~ifelse(stringr::str_length(.) == 0, NA, .)) %>%
+    dplyr::mutate(r2 = ifelse(is.na(r1) & is.na(r2) & is.na(r3), "drop", r2)) %>%
+    tidyr::fill(r1, r2) %>%
+    tidyr::unite(header, sep = ".") %>%
+    dplyr::mutate(header = ifelse(duplicated(.), paste(header, dplyr::row_number(), sep = "_"), header)) %>%
+    dplyr::pull()
+
+  return(headers)
+}
--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 
 OHA High Frequency Reporting Munging
 
+![workflow](https://user-images.githubusercontent.com/8933069/56661405-abb9e300-666f-11e9-8a25-eb9e9c370c4a.png)
+
+
 The [work plan](https://docs.google.com/document/d/10kLZofiJE4ueDOoOIJ49SIdT6iFK3VVm9J3_DdSpW0A/edit?usp=sharing) as well as futher documentation and data can be found on [Google Drive](https://drive.google.com/open?id=14lcqRwZaR7ZhyhF2-NoAwuhF6p4dhVzc).
 
 Notes about each of the countries can be found in the [wiki](https://github.com/USAID-OHA-SI/Wavelength/wiki).

8d22fcc72735e9ba857d875df227a854cf17fe52
Aaron Chafetz <achafetz@usaid.gov>
2019-04-25 16:42:11 -0400

    adjust coding to deal with new structure

--- R/structure_uga.R
+++ R/structure_uga.R
@@ -21,29 +21,33 @@
 
 structure_uga <- function(filepath, folderpath_output = NULL){
 
-  #apply manually created var names
-    meta <- c("reporting_pd", "snu1", "psnu", "community", "facility", "orgunituid", "partner")
-    varnames <- dplyr::pull(ind_map_uga, header)
-
-  #import dataset (without names)
-    df <- readxl::read_excel(filepath, skip = 3, col_types = "text",
-                             col_names = c(meta, varnames))
+  #import dataset
+    df <- readr::read_csv("ou_submissions/Data Dump Results - Weekly16.csv")
 
   #check structure
     #TODO add assert check to make sure structure stays the same
 
-  #remove columns for data OHA is not collecting
-    df <- dplyr::select(df, -dplyr::starts_with("drop"))
+  #map indicator/disagg names onto dataframe
+    df <- dplyr::full_join(df, ind_map_uga,
+                           by = c("DataElement.name" = "indicator_orig",
+                                  "DISAGG" = "disaggregate_orig"))
 
-  #reshape long, dropping all zeros
-    df <- reshape_long(df, meta)
+  #remove indicators/columns OHA is not collecting
+    df <- df %>%
+      dplyr::select(reporting_pd = `Report period`,
+                    snu1 = region, psnu = district,
+                    community = subcounty, facility = Outlet,
+                    orgunituid = Outlet.uid, partner = IM,
+                    indicator:modality, val = value) %>%
+      dplyr::filter(stringr::str_detect(indicator, "drop", negate = TRUE),
+                    !val %in% c(0, NA))
 
   #add operatingunit
     df <- add_ou(df, "Uganda")
 
   #add reporting frequency
     df <- tibble::add_column(df, reporting_freq = "Weekly",
-                             .after = "partner")
+                           .after = "partner")
 
   #break out reporting pd & add month and FY
     df <- df %>%
@@ -54,10 +58,9 @@ structure_uga <- function(filepath, folderpath_output = NULL){
                       stringr::str_sub(., 1, 4)) %>%
       dplyr::select(-val, dplyr::everything())
 
-  #breakout indicator and disaggs
+  #cleanup indicator and disaggs
     components <- c("indicator", "disaggregate", "agecoarse", "agesemifine", "sex", "modality")
     df <- df %>%
-      tidyr::separate(ind, components, sep = "\\.", fill = "right") %>%
       dplyr::mutate_at(dplyr::vars(dplyr::one_of(components)), ~ dplyr::na_if(., "NA")) %>%
       dplyr::mutate(sex = ifelse(sex == "Peds", NA, sex))
 
@@ -66,5 +69,5 @@ structure_uga <- function(filepath, folderpath_output = NULL){
   #export
     export_hfd(df, folderpath_output)
 
-    return(df)
+  return(df)
 }

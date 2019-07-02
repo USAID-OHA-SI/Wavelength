be72e2941eb75d418c5f809fcd91b2fcabf4925a
Aaron Chafetz <achafetz@usaid.gov>
2019-07-02 10:25:58 -0400

    restructure with new output from TZA

--- R/structure_tza.R
+++ R/structure_tza.R
@@ -3,12 +3,10 @@
 #' @description This function will be used to take the monthly Tanzania
 #' dataset and structure in a uniform way to be tidy and align with other
 #' OU datasets. Only selected indicators will be imported and transformed
-#' based on OHA's needs. The data is sourced from Tazania's monthly reporting
-#' platform \url{http://hmis.reachproject.or.tz/MonthlyPortal/}
+#' based on OHA's needs.
 #'
 #' @param filepath filepath to the output file
 #' @param folderpath_output if a txt output is desired, provide the full path to the folder
-#' @param type is the dataset a weekly or monthly file, "weekly", "monthly"
 #'
 #' @export
 #'
@@ -20,104 +18,47 @@
 #' #structure output for Tanzania & export to txt file
 #'   structure_tza(path, "~/WeeklyData/Output") }
 
-structure_tza <- function(filepath, type = "weekly", folderpath_output = NULL){
-
-  if(type == "weekly") {
-
-    #identify variables to keep
-      sel_ind <- dplyr::pull(ind_map_tza, ind_label)
-
-    #unwanted tabs (don't have site data) "Facility Data", "Community Data",
-      remove <- c("Guide-Facility", "Guide-Comm", "flat file", "All 40 Sites")
-
-    #import all site sheets
-      df <- filepath %>%
-        readxl::excel_sheets() %>%
-        setdiff(remove) %>%
-        purrr::set_names() %>%
-        purrr::map_dfr(~readxl::read_excel(filepath, sheet = .x, skip = 3, col_types = "text"))
-
-    #filter to select indicators
-      df <- df %>%
-        dplyr::select(`Start date`:`Site ID (from DATIM)`, dplyr::one_of(sel_ind), -`End date`)
-
-    #munge and reshape long
-      df <- df %>%
-        # dplyr::select(-dplyr::matches("\\...")) %>% #get rid of extra blank columns at end
-        dplyr::select(`Start date`:`Site ID (from DATIM)`, dplyr::one_of(sel_ind)) %>%
-        dplyr::mutate_at(dplyr::vars(`Start date`),
-                         ~ as.double(.) %>% as.Date(origin = "1899-12-30")) #convert date
-      meta <- dplyr::select(df, `Start date`:`Site ID (from DATIM)`) %>% names()
-      df <- reshape_long(df, meta)
-
-    #clean names
-      df <- df %>%
-        dplyr::rename(date = `Start date`,
-                      site = `Site Name`,
-                      orgunituid = `Site ID (from DATIM)`,
-                      fundingagency = Agency) %>%
-        dplyr::rename_all(tolower) %>%
-        dplyr::mutate(site = stringr::str_remove_all(site, " -.*$| \\."))
-
-    #map indicator name on & filter to just OHA select values
-      df <- df %>%
-        dplyr::left_join(ind_map_tza, by =  c("ind" = "ind_label")) %>%
-        dplyr::select(-ind, -ind.y) %>%
-        dplyr::select(-val, dplyr::everything())
-
-    #add reporting pd
-      df <- tibble::add_column(df, reporting_freq = "Weekly",
-                               .before = "indicator")
-
-  } else if(type == "monthly"){
+structure_tza <- function(filepath, folderpath_output = NULL){
 
   #import
-    df <- readxl::read_excel(filepath, sheet = "Exported Data")
-
-  #check structure
-    #TODO add assert check to make sure structure stays the same
-
-  #identify variables to keep
-    sel_ind <- dplyr::pull(ind_map_tza_old, ind)
+    df <- readxl::read_xlsx(filepath, sheet = "Weekly", col_type = "text")
 
   #filter to select indicators
+    sel_ind <- dplyr::pull(ind_map_tza, ind_label)
     df <- df %>%
-      dplyr::select(Partner:Month, dplyr::one_of(sel_ind)) %>%
-      dplyr::rename_all(~ tolower(.)) %>%
-      dplyr::rename(orgunituid = `datim id`)
+      dplyr::select(`Start date`:`Site ID (from DATIM)`, dplyr::one_of(sel_ind), -`End date`)
 
-  #reshape long and convert val to numeric
-    meta <- dplyr::select(df, partner:month) %>% names()
+  #fix date
+    df <- df %>%
+      dplyr::mutate(date = as.Date(as.double(`Start date`), origin = "1899-12-30")) %>%
+      dplyr::select(date, dplyr::everything(), -`Start date`)
+
+  #munge and reshape long
+    meta <- dplyr::select(df, date:`Site ID (from DATIM)`) %>% names()
     df <- reshape_long(df, meta)
 
-   #add reporting frequency
-    df <- tibble::add_column(df, reporting_freq = "Monthly",
-                             .before = "ind")
-
-   #map to standardized indicators
+  #clean names
     df <- df %>%
-      dplyr::left_join(ind_map_tza_old, by = "ind") %>%
-      dplyr::select(-ind) %>%
+      dplyr::rename(facility = `Site Name`,
+                    orgunituid = `Site ID (from DATIM)`,
+                    snu1 = Region,
+                    psnu = Council,
+                    community = Ward,
+                    fundingagency = Agency) %>%
+      dplyr::select(-District) %>%
+      dplyr::rename_all(tolower) %>%
+      dplyr::mutate(facility = stringr::str_remove_all(facility, " -.*$| \\."))
+
+  #map indicator name on & filter to just OHA select values
+    df <- df %>%
+      dplyr::left_join(ind_map_tza, by =  c("ind" = "ind_label")) %>%
+      dplyr::select(-ind, -ind.y) %>%
       dplyr::select(-val, dplyr::everything())
 
-  #convert month into date
-    df <- df %>%
-      dplyr::mutate(date = stringr::str_replace(month, " ", " 1,") %>% lubridate::mdy()) %>%
-      dplyr::select(date, dplyr::everything()) %>%
-      dplyr::select(-month)
-
-  #rename agency to match others
-    df <- dplyr::rename(df, fundingagency = agency)
-
-  }
-
-  #geo hierarchy alignment
-    df <- df %>%
-      dplyr::rename(snu1 = region,
-                    psnu = council,
-                    community = ward,
-                    facility = site) %>%
-      dplyr::select(-district)
+  #add reporting pd
+    df <- tibble::add_column(df, reporting_freq = "Weekly",
+                             .before = "indicator") %>%
+      dplyr::mutate(reporting_freq = ifelse(indicator == "TX_CURR", "Monthly", reporting_freq))
 
   #add FY
     df <- df %>%
@@ -134,6 +75,6 @@ structure_tza <- function(filepath, type = "weekly", folderpath_output = NULL){
   #export
     export_hfr(df, folderpath_output)
 
-    return(df)
+  invisible(df)
 
 }

fe0001adab109365502bb8617c1aa7fde73e86e1
Aaron Chafetz <achafetz@usaid.gov>
2019-07-02 10:25:39 -0400

    update selected vars to remove SDI and Index

--- data-raw/ind_map.R
+++ data-raw/ind_map.R
@@ -18,13 +18,13 @@ library(usethis)
 
   #WEEKLY
   #import indicator mapping
-  ind_map_tza <- read_csv("data-raw/ind_map_tza.csv")
+  ind_map_tza <- readr::read_csv("data-raw/ind_map_tza.csv")
 
   #filter all indicators collected to ones that we currently want to review
-  ind_map_tza <- filter(ind_map_tza, !is.na(indicator))
+  ind_map_tza <- dplyr::filter(ind_map_tza, !is.na(indicator))
 
   #save to data
-  use_data(ind_map_tza, overwrite = TRUE)
+  usethis::use_data(ind_map_tza, overwrite = TRUE)
 
 # UGANDA ------------------------------------------------------------------
 

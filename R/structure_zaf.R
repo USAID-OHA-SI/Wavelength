7d85253e13f40b754c4a386b6b323937437fcb54
Aaron Chafetz <achafetz@usaid.gov>
2019-07-03 17:06:36 -0400

    add URC import to ZAF

--- R/structure_zaf.R
+++ R/structure_zaf.R
@@ -21,33 +21,48 @@
 
 structure_zaf <- function(filepath, folderpath_output = NULL){
 
-  #import
-    df <- readr::read_csv(filepath, col_types = c(.default = "c"))
+  if(stringr::str_detect(filepath, "URC")){
 
-  #limit columns & conver to lower case
-    df <- df %>%
-      dplyr::select(FundingAgency:`Sub-district`, Facility, date = Week_Start, Indicator, val = Value) %>%
-      dplyr::rename_all(tolower)
+    #import
+      df <- readxl::read_xlsx(filepath, col_types = "text")
 
-  #filter to select indicators
-    ind_sel <- c("HTS_TST","HTS_TST_POS", "TX_NEW", "TX_CURR_28")
-    df <- df %>%
-      dplyr::filter(indicator %in% ind_sel) %>%
-      dplyr::mutate(indicator = stringr::str_remove(indicator, "_28"))
+    #convert columns to lower case and reshape
+      df <- dplyr::rename_all(df, tolower)
+      meta <- dplyr::select(df, fundingagency:facility) %>% names()
+      df <- reshape_long(df, meta) %>%
+        dplyr::rename(date = ind)
+    #format date
+      df <- dplyr::mutate(df, date = lubridate::as_date(as.integer(date), origin = "1899-12-30"))
+  } else {
+    #import
+      df <- readr::read_csv(filepath, col_types = c(.default = "c"))
+
+    #limit columns & conver to lower case
+      df <- df %>%
+        dplyr::select(FundingAgency:`Sub-district`, Facility, date = Week_Start, Indicator, val = Value) %>%
+        dplyr::rename_all(tolower)
+
+    #filter to select indicators
+      ind_sel <- c("HTS_TST","HTS_TST_POS", "TX_NEW", "TX_CURR_28")
+      df <- df %>%
+        dplyr::filter(indicator %in% ind_sel) %>%
+        dplyr::mutate(indicator = stringr::str_remove(indicator, "_28"))
+
+    #format date
+      df <- dplyr::mutate(df, date = lubridate::mdy(date))
+
+  }
 
   #align geography
     df <- df %>%
+      add_ou("South Africa") %>%
       dplyr::rename(snu1 = province,
                     psnu = district,
                     community = `sub-district`)
 
-  #add operating unit
-    df <- add_ou(df, "South Africa")
-
   #format date and value
     df <- df %>%
-      dplyr::mutate(date = lubridate::mdy(date),
-                    fy = lubridate::quarter(date, with_year = TRUE, fiscal_start = 10) %>%
+      dplyr::mutate(fy = lubridate::quarter(date, with_year = TRUE, fiscal_start = 10) %>%
                       stringr::str_sub(., 1, 4),
                     val = as.numeric(val))
 

13c628561d3fe2f3e8f0fe132008a967b0a57bc8
Aaron Chafetz <achafetz@usaid.gov>
2019-07-23 16:01:09 -0400

    work

--- data-raw/scrapwork.R
+++ data-raw/scrapwork.R
@@ -35,7 +35,12 @@ get_em <- function(ou, folderpath_output = "C:/Users/achafetz/Downloads"){
 
 get_em("Malawi")
 
-purrr::walk(ous, get_em)
+purrr::walk(ous, ~extract_datim(ou_name = .x,
+                                  username = myuser,
+                                  password = mypwd(myusername),
+                                  folderpath_output = "out/"))
+
+
 
 files <- list.files("C:/Users/achafetz/Downloads", "HFR_DATIM", full.names = TRUE)
 
@@ -46,52 +51,6 @@ filename <- paste0("HFR_DATIM_FY19Q2_Global_", format(Sys.Date(),"%Y%m%d"), ".tx
 readr::write_tsv(full_set, file.path(folderpath_output, filename), na = "")
 
 
-# Testing -----------------------------------------------------------------
-
-
-combine_sources <- function(path_hfr, path_datim, start_date, hfr_pd, weeks = 4, output_folder = "out/joint"){
-
-  #import
-  if(tools::file_ext(path_hfr) == "txt"){
-    df_hfr <- readr::read_tsv(path_hfr, col_types = c(.default = "c")) %>%
-      dplyr::mutate(date = lubridate::ymd(date))
-  } else {
-    df_hfr <- readr::read_csv(path_hfr, col_types = c(.default = "c")) %>%
-      dplyr::mutate(date = lubridate::mdy(date))
-  }
-
-  if("site" %in% names(df_hfr)) df_hfr <- dplyr::rename(df_hfr, facility = site)
-
-  df_mer <- readr::read_tsv(path_datim, col_types = c(.default = "c"))
-
-  #rename org/partner info (HFR)
-    # vars_dup <- c("operatingunit", "fundingagency", "snu1", "psnu", "facility") #"partner"
-    # if("community" %in% names(df_hfr)) vars_dup <- c(vars_dup, "community")
-    # vars_rename <- c("partner")
-
-    df_hfr_adj <- df_hfr #%>%
-      # dplyr::select(-vars_dup) %>%
-      # dplyr::rename_at(dplyr::vars(vars_rename), ~ paste0(., "_hfr"))
-
-  #duplicate targets for each week (DATIM)
-    dates <- lubridate::as_date(start_date) %>% seq(by = 7, length.out = weeks)
-
-    df_mer_rpt <- purrr::map_dfr(.x = dates,
-                   .f = ~dplyr::mutate(df_mer, date = .x))
-
-  #join
-    df_join <- dplyr::full_join(df_mer_rpt, df_hfr_adj) # , by = c("orgunituid", "fy", "mechanismid", "agecoarse", "sex", "indicator", "date"))
-
-  #export
-    ou <- df_join$operatingunit[1]
-    if(stringr::str_detect(ou, "Reg")) ou <- df_join$snu1[1]
-    iso <- dplyr::filter(iso_map, countryname == ou) %>%
-      dplyr::pull(iso)
-
-    filename <- paste0("HFR_JOINT_", iso, "_FY19P", hfr_pd, ".txt") %>% file.path(output_folder, .)
-    readr::write_tsv(df_join, filename, na = "")
-
-}
 
 
 

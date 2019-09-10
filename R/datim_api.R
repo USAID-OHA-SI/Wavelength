382d85e91303bd2f860b23c16b11f578dd2b3e66
Aaron Chafetz <achafetz@usaid.gov>
2019-09-10 13:01:12 -0400

    adjust period to Q3 for saving

--- R/datim_api.R
+++ R/datim_api.R
@@ -497,7 +497,7 @@
         iso <- dplyr::filter(iso_map, countryname == ou_name) %>%
           dplyr::pull(iso)
 
-        filename <- paste0("HFR_DATIM_FY19Q2_", iso, "_", format(Sys.Date(),"%Y%m%d"), ".txt")
+        filename <- paste0("HFR_DATIM_FY19Q3_", iso, "_", format(Sys.Date(),"%Y%m%d"), ".txt")
 
         readr::write_tsv(df_combo, file.path(folderpath_output, filename), na = "")
       }

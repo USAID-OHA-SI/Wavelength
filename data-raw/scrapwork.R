a7146e5de19d6e7fb181b4e8f392569591c4c40b
Aaron Chafetz <achafetz@usaid.gov>
2019-09-19 16:47:38 -0400

    update and clean up work to reflect workflow

--- data-raw/scrapwork.R
+++ data-raw/scrapwork.R
@@ -1,108 +1,193 @@
 # Pull from DATIM ---------------------------------------------------------
 
+  ## NOTE: Needs to be updated each quarter for updated Results/Targets/Gap Target
+
+  #DATIM username
+    myuser <- ""
+
+  #identify full set of OU/country list for all USAID targets
+    ous <- identify_levels(username = myuser, password = mypwd(myuser)) %>%
+      dplyr::filter(is.na(name4)) %>%
+      dplyr::pull(country_name)
 
 
-ous <- c("Angola", "Botswana", "Burma", "Burundi", "Cameroon",
-         "Cote d'Ivoire", "Democratic Republic of the Congo",
-         "Dominican Republic", "Eswatini", "Ethiopia",
-         "Ghana", "Haiti", "India", "Indonesia",
-         "Kenya", "Lesotho", "Malawi", "Mozambique",
-         "Namibia", "Nigeria", "Papua New Guinea", "Rwanda",
-         "South Africa", "South Sudan", "Tanzania", "Uganda",
-         "Ukraine", "Vietnam", "Zambia", "Zimbabwe")
+    ous <- identify_levels(username = myuser, password = mypwd(myuser)) %>%
+      dplyr::filter(!is.na(name4)) %>%
+      dplyr::pull(country_name) %>%
+      sort()
 
-ous <- c("Zambia", "Zimbabwe")
+    ous <- c('Guatemala', "Hondura",
+             "Nicaragua", "Panama")
 
-
-
-get_em <- function(ou, folderpath_output = "C:/Users/achafetz/Downloads"){
-  df <- extract_targets(ou_name = ou, username = myuser, password = mypwd(myuser))
-
-  if(!is.null(df)){
-    df <- periodize_targets(df, 2)
-
-    iso <- dplyr::filter(iso_map, countryname == ou) %>%
-      dplyr::pull(iso)
-
-    filename <- paste0("HFR_DATIM_FY19Q2_", ou, "_", format(Sys.Date(),"%Y%m%d"), ".txt")
-
-    readr::write_tsv(df, file.path(folderpath_output, filename), na = "")
-
-    invisible(df)
-  }
-
-}
-
-get_em("Malawi")
-
-purrr::walk(ous, ~extract_datim(ou_name = .x,
-                                  username = myuser,
-                                  password = mypwd(myusername),
-                                  folderpath_output = "out/"))
+  #extract and save targets for all OUs
+    purrr::walk(ous, ~extract_datim(ou_name = .x,
+                                    username = myuser,
+                                    password = mypwd(myuser),
+                                    quarters_complete = 3,
+                                    folderpath_output = "out/DATIM"))
 
 
+# Combine HFR & DATIM -----------------------------------------------------
 
-files <- list.files("C:/Users/achafetz/Downloads", "HFR_DATIM", full.names = TRUE)
+  ## NOTE: this creates a global file,
 
+  #folder where all HFR processed files are stored
+    path <- "../Downloads/HFR_PD11n"
 
-full_set <- purrr::map_dfr(files, readr::read_tsv, col_types = c(.default = "c"))
-folderpath_output <- "C:/Users/achafetz/Downloads"
-filename <- paste0("HFR_DATIM_FY19Q2_Global_", format(Sys.Date(),"%Y%m%d"), ".txt")
-readr::write_tsv(full_set, file.path(folderpath_output, filename), na = "")
-
-
-
-
-
-path_datim <- list.files("out/DATIM", "FY19Q2_Zam", full.names = TRUE) %>% fs::path_abs()
-path_hfr <- list.files("out/processed", "ZMB", full.names = TRUE)[2] %>% fs::path_abs()
-
-x <- combine_sources(path_hfr, path_datim, "2019-06-10", 10)
-x <- combine_sources(path_hfr, path_datim, "2019-05-13", 9)
+  #append DATIM onto HFR (map so completes for all OUs in folderpath_hfr)
+    df <- append_sources(folderpath_hfr = path,
+                         folderpath_datim = "out/DATIM",
+                         start_date = "2019-06-03",
+                         weeks = 9,
+                         max_date = "2019-07-29",
+                         folderpath_output = "out/joint")
 
 
-x2 <- list.files("out/joint", "ZMB", full.names = TRUE) %>%
-  purrr::map_dfr(readr::read_tsv, col_type = c(.default = "c"))
+# Integrity Checks --------------------------------------------------------
 
-x2 %>%
-  dplyr::mutate_at(dplyr::vars(mer_results, mer_targets, weekly_targets,
-                               targets_gap, weekly_targets_gap, val), as.numeric) %>%
-  dplyr::group_by_if(is.character) %>%
-  dplyr::summarise_if(is.numeric, sum, na.rm = TRUE) %>%
-  dplyr::ungroup() %>%
-  readr::write_tsv("C:/Users/achafetz/Documents/GitHub/Wavelength/out/Processed/HFR_JOINT_ZMB_FY19P09-10.txt", na = "")
+  #check headers
+    names(df)
+
+    #included mecahnism ids?
+    check_mechid <- function(df){
+
+      total <- df %>%
+        dplyr::distinct(mechanismid, partner) %>%
+        nrow()
+
+      missing <- df %>%
+        dplyr::distinct(mechanismid, partner) %>%
+        dplyr::filter(is.na(mechanismid)) %>%
+        nrow()
+
+      if(missing == 0){
+        print("All partners have mechanism IDs")
+      } else {
+        print(paste0(missing, " of the ", total, " mechanisms are missing DATIM mechanism IDs." ))
+      }
+    }
+
+    #included facility uids
+    check_orgunituid <- function(df){
+
+      total <- df %>%
+        dplyr::distinct(facility, orgunituid) %>%
+        nrow()
+
+      missing <- df %>%
+        dplyr::distinct(facility, orgunituid) %>%
+        dplyr::filter(is.na(orgunituid)) %>%
+        nrow()
+
+      if(missing == 0){
+        print("All partners have sites/communities UIDs")
+      } else {
+        print(paste0(missing, " of the ", total, " sites/communities are missing DATIM UIDs." ))
+      }
+    }
+
+    #indicators
+    check_ind <- function(df){
+
+      df_fullset <- tibble::tribble(
+        ~indicator, ~agecoarse,     ~sex, ~otherdisaggregate,
+        "HTS_TST",      "<15", "Female",                 NA,
+        "HTS_TST",      "15+", "Female",                 NA,
+        "HTS_TST",      "<15",   "Male",                 NA,
+        "HTS_TST",      "15+",   "Male",                 NA,
+        "HTS_TST_POS",      "<15", "Female",                 NA,
+        "HTS_TST_POS",      "15+", "Female",                 NA,
+        "HTS_TST_POS",      "<15",   "Male",                 NA,
+        "HTS_TST_POS",      "15+",   "Male",                 NA,
+        "TX_NEW",      "<15", "Female",                 NA,
+        "TX_NEW",      "15+", "Female",                 NA,
+        "TX_NEW",      "<15",   "Male",                 NA,
+        "TX_NEW",      "15+",   "Male",                 NA,
+        "TX_CURR",      "<15", "Female",                 NA,
+        "TX_CURR",      "15+", "Female",                 NA,
+        "TX_CURR",      "<15",   "Male",                 NA,
+        "TX_CURR",      "15+",   "Male",                 NA,
+        "VMMC_CIRC",      "<15",   "Male",                 NA,
+        "VMMC_CIRC",      "15+",   "Male",                 NA,
+        "PrEP_NEW",      "15+", "Female",                 NA,
+        "PrEP_NEW",      "15+",   "Male",                 NA,
+        "TX_MMD",         NA,       NA,          "1 month",
+        "TX_MMD",         NA,       NA,         "2 months",
+        "TX_MMD",         NA,       NA,         "3 months",
+        "TX_MMD",         NA,       NA,       "4-5 months",
+        "TX_MMD",         NA,       NA, "6 or more months"
+      )
+
+      missing <- df_fullset %>%
+        dplyr::distinct(indicator) %>%
+        dplyr::pull() %>%
+        setdiff(unique(df$indicator)) %>%
+        paste(collapse = ", ")
+
+      if(missing == 0){
+        print("No indicators are missing.")
+      } else {
+        print(paste0("Missing indicators: ", missing))
+      }
+    }
+
+    #check # of sites reporting each indicators and disagg
+    check_inddisagg <- function(df, pd_start_date){
+
+      df_fullset <- tibble::tribble(
+        ~indicator, ~agecoarse,     ~sex, ~otherdisaggregate,
+        "HTS_TST",      "<15", "Female",                 NA,
+        "HTS_TST",      "15+", "Female",                 NA,
+        "HTS_TST",      "<15",   "Male",                 NA,
+        "HTS_TST",      "15+",   "Male",                 NA,
+        "HTS_TST_POS",      "<15", "Female",                 NA,
+        "HTS_TST_POS",      "15+", "Female",                 NA,
+        "HTS_TST_POS",      "<15",   "Male",                 NA,
+        "HTS_TST_POS",      "15+",   "Male",                 NA,
+        "TX_NEW",      "<15", "Female",                 NA,
+        "TX_NEW",      "15+", "Female",                 NA,
+        "TX_NEW",      "<15",   "Male",                 NA,
+        "TX_NEW",      "15+",   "Male",                 NA,
+        "TX_CURR",      "<15", "Female",                 NA,
+        "TX_CURR",      "15+", "Female",                 NA,
+        "TX_CURR",      "<15",   "Male",                 NA,
+        "TX_CURR",      "15+",   "Male",                 NA,
+        "VMMC_CIRC",      "<15",   "Male",                 NA,
+        "VMMC_CIRC",      "15+",   "Male",                 NA,
+        "PrEP_NEW",      "15+", "Female",                 NA,
+        "PrEP_NEW",      "15+",   "Male",                 NA,
+        "TX_MMD",         NA,       NA,          "1 month",
+        "TX_MMD",         NA,       NA,         "2 months",
+        "TX_MMD",         NA,       NA,         "3 months",
+        "TX_MMD",         NA,       NA,       "4-5 months",
+        "TX_MMD",         NA,       NA, "6 or more months"
+      )
+
+      pd_start_date <- as.Date(pd_start_date)
+      pd_end_date <- as.Date(pd_start_date) + 28
+
+      df %>%
+        dplyr::group_by(facility, orgunituid, indicator, agecoarse, sex, otherdisaggregate, date) %>%
+        dplyr::summarise_at(dplyr::vars(val), sum, na.rm = TRUE) %>%
+        dplyr::ungroup() %>%
+        dplyr::filter(date >= pd_start_date,
+                      date <=  pd_end_date) %>%
+        dplyr::count(indicator, agecoarse, sex, otherdisaggregate, date) %>%
+        tidyr::spread(date, n) %>%
+        dplyr::full_join(df_fullset, ., by = c("indicator", "agecoarse", "sex", "otherdisaggregate")) %>% #"otherdisaggregate"
+        dplyr::mutate(disagg = dplyr::case_when(indicator == "TX_MMD" ~ otherdisaggregate,
+                                                TRUE                  ~ paste(sex, agecoarse, sep = "/"))) %>%
+        dplyr::select(-agecoarse, -sex, -otherdisaggregate) %>%
+        dplyr::select(indicator, disagg, dplyr::everything()) %>%
+        print(n = Inf)
+    }
 
 
-all <- list.files("out/joint/", full.names = TRUE) %>%
-  purrr::map_dfr(readr::read_tsv, col_types = c(.default = "c"))
+    check_mechid(df)
+    check_orgunituid(df)
+    check_ind(df)
+    check_inddisagg(df, "2019-05-13")
 
-#fix dates (Moz & Ukraine)
-all <- all %>%
-  dplyr::mutate(date = dplyr::case_when(date == "2019-06-20" ~ "2019-06-03",
-                                        date == "2019-05-01" ~ "2019-04-29",
-                                        TRUE ~ date))
-
-#fix MMD indicator (MOZ)
-all <- all %>%
-  dplyr::mutate(otherdisaggregate = ifelse(indicator == "3MD", "3 months", otherdisaggregate),
-                disaggregate = ifelse(indicator == "3MD", "Period", disaggregate),
-                indicator = ifelse(indicator %in% c("3MD", "TX_MMS"), "TX_MMD", indicator))
-#adjust age and sex to be unknown for DATIM
-all <- all %>%
-  dplyr::mutate(sex = ifelse(sex == "Unspecified sex", "Unknown", sex),
-                agecoarse = ifelse(agecoarse == "Unknown Age", "Unknown", agecoarse))
-
-all <- all %>%
-  dplyr::mutate_at(dplyr::vars(val, mer_results, mer_targets, weekly_targets, targets_gap, weekly_targets_gap), as.numeric) %>%
-  dplyr::mutate(agecoarse = ifelse(!is.na(age), age, agecoarse),
-                orgunituid = ifelse(!is.na(facilityuid), facilityuid, orgunituid),
-                date = lubridate::as_date(date)) %>%
-  dplyr::filter(date < "2019-06-10") %>%
-  dplyr::select(-c(facilityuid, age, `Pivot1 Names`))
-
-readr::write_tsv(all, paste0("C:/Users/achafetz/Downloads/HFR_ready/HFR_output_", format(Sys.time(),"%Y%m%d.%H%M"), ".txt"), na = "")
-
-dplyr::mutate(df_hfr, date = lubridate::mdy(date))
 
 
 

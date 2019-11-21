063294fd71476aa78e4c8aaa4220563bfe734165
Aaron Chafetz <achafetz@usaid.gov>
2019-11-21 12:35:53 -0500

    add handing of mdy character for fix_date

--- R/fix_date.R
+++ R/fix_date.R
@@ -16,13 +16,18 @@ fix_date <- function(df){
       dplyr::filter(stringr::str_detect(date,  "[:digit:]{4}-")) %>%
       dplyr::mutate(date = as.Date(date))
 
+  #adjust character date, assuming format is mdy
+    df_date_mdy <- df %>%
+      dplyr::filter(stringr::str_detect(date,  "[:digit:]{2}(-|/)")) %>%
+      dplyr::mutate(date = lubridate::mdy(date))
+
   #replace other dates as NA
     df_date_other <- df %>%
-      dplyr::filter(stringr::str_detect(date,  "[:digit:]{4}-|[:digit:]{5}", negate = TRUE)) %>%
+      dplyr::filter(stringr::str_detect(date, "[:digit:]{4}-|[:digit:]{5}|[:digit:]{2}(-|/)", negate = TRUE)) %>%
       dplyr::mutate(date = as.Date(NA))
 
   #bind all types together
-    df_date_fixed <- dplyr::bind_rows(df_date_excel, df_date_iso, df_date_other)
+    df_date_fixed <- dplyr::bind_rows(df_date_excel, df_date_iso, df_date_mdy, df_date_other)
 
   return(df_date_fixed)
 }

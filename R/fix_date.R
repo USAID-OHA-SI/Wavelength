8420f8584e12c0d12201fa9edd328fe24221d0d9
Aaron Chafetz <achafetz@usaid.gov>
2019-11-22 10:14:04 -0500

    added a rounding option to fix_date, default is to not round

--- R/fix_date.R
+++ R/fix_date.R
@@ -1,10 +1,11 @@
 #' Convert dates to date format
 #'
 #' @param df HFR data frame imported via `import_hfr()`
+#' @param round_date rounds date to the nearest HFRweek start (for non-compliance), default = FALSE
 #'
 #' @export
 
-fix_date <- function(df){
+fix_date <- function(df, round_date = FALSE){
 
   #adjust Excel formated dates
     df_date_excel <- df %>%
@@ -29,5 +30,30 @@ fix_date <- function(df){
   #bind all types together
     df_date_fixed <- dplyr::bind_rows(df_date_excel, df_date_iso, df_date_mdy, df_date_other)
 
+  #round date (fixes non-compliance)
+   if(round_date == TRUE)
+     df_date_fixed <- round_date(df_date_fixed)
+
   return(df_date_fixed)
+}
+
+#' Round to nearest HFR date
+#'
+#' @param df df HFR data frame imported via `import_hfr()`
+#'
+#' @export
+
+round_date <- function(df){
+  if(var_exists(df, date)){
+    df <-
+      dplyr::mutate(date = dplyr::case_when(is.na(date) ~ as.Date(NA),
+                                            lubridate::wday(date) == 1 ~
+                                              lubridate::ceiling_date(date, unit = "week",
+                                                                      week_start = 1),
+                                            lubridate::wday(date)  > 1 ~
+                                              lubridate::floor_date(date, unit = "week",
+                                                                    week_start = 1)))
+  }
+
+  return(df)
 }

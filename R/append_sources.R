e6f5c7a3c74612aece52470d71074a658774f208
Aaron Chafetz <achafetz@usaid.gov>
2019-11-23 12:24:10 -0500

    update w/ var name changes and address partner DATIM issue

--- R/append_sources.R
+++ R/append_sources.R
@@ -42,18 +42,41 @@ append_sources <- function(folderpath_hfr,
     dplyr::select(-dplyr::matches("Type of organisational unit"))
 
 
-  # MAP MECHANISM INFORMATION -----------------------------------------------
+# EXTRACT MISSING PARTNER NAME --------------------------------------------
+
+  if(!"primepartner" %in% names(df_datim)){
+    #access current mechanism list posted publically to DATIM
+    sql_view_url <- "https://www.datim.org/api/sqlViews/fgUtV6e9YIX/data.csv"
+    mech_official <- readr::read_csv(sql_view_url,
+                                     col_types = readr::cols(.default = "c"))
+
+    mech_info <- mech_official %>%
+      dplyr::filter(agency == "USAID") %>%
+      dplyr::select(mech_code = code, primepartner_d = partner)
+
+    df_datim <- df_datim %>%
+      tibble::add_column(primepartner = as.character(NA), .before = "mech_code") %>%
+      dplyr::left_join(mech_info, by = "mech_code") %>%
+      dplyr::mutate(primepartner = primepartner_d) %>%
+      dplyr::select(-primepartner_d) %>%
+      dplyr::glimpse()
+
+    rm(sql_view_url, mech_official, mech_info)
+  }
+
+
+# MAP MECHANISM INFORMATION -----------------------------------------------
 
   #pull list of mechanism from DATIM targets
   df_mech_map <- df_datim %>%
-    dplyr::distinct(mechanismid, primepartner, implementingmechanismname) %>%
-    dplyr::rename_at(dplyr::vars(primepartner, implementingmechanismname), ~ paste0(., "_d"))
+    dplyr::distinct(mech_code, primepartner, mech_name) %>%
+    dplyr::rename_at(dplyr::vars(primepartner, mech_name), ~ paste0(., "_d"))
 
-  #merge on mechanismid, replacing HFR with DATIM names
+  #merge on mech_code, replacing HFR with DATIM names
   df_hfr <- df_hfr %>%
-    dplyr::left_join(df_mech_map, by = "mechanismid") %>%
+    dplyr::left_join(df_mech_map, by = "mech_code") %>%
     dplyr::mutate(primepartner = primepartner_d,
-                  implementingmechanismname = implementingmechanismname_d) %>%
+                  mech_name = mech_name_d) %>%
     dplyr::select(-dplyr::ends_with("_d"))
 
   rm(df_mech_map)
@@ -69,7 +92,7 @@ append_sources <- function(folderpath_hfr,
   df_hfr_orguids <- df_hfr %>%
     dplyr::filter(!is.na(orgunituid)) %>%
     dplyr::left_join(df_org_map, by = c("orgunituid" = "orgunituid_d")) %>%
-    dplyr::mutate(snu1 = ifelse(!is.na(snu1_d), snu1_d, snu1),
+    dplyr::mutate(#snu1 = ifelse(!is.na(snu1_d), snu1_d, snu1),
                   psnu = ifelse(!is.na(psnu_d), psnu_d, psnu),
                   orgunit = ifelse(!is.na(orgunit_d), orgunit_d, orgunit)) %>%
     dplyr::select(-dplyr::ends_with("_d"))
@@ -79,7 +102,7 @@ append_sources <- function(folderpath_hfr,
   df_hfr_orguids_missing <- df_hfr %>%
     dplyr::filter(is.na(orgunituid)) %>%
     dplyr::left_join(df_org_map_missing, by = c("orgunit" = "orgunit_d")) %>%
-    dplyr::mutate(snu1 = ifelse(!is.na(snu1_d), snu1_d, snu1),
+    dplyr::mutate(#snu1 = ifelse(!is.na(snu1_d), snu1_d, snu1),
                   psnu = ifelse(!is.na(psnu_d), psnu_d, psnu),
                   orgunituid = orgunituid_d) %>%
     dplyr::select(-dplyr::ends_with("_d"))
@@ -91,22 +114,22 @@ append_sources <- function(folderpath_hfr,
 
   # REMOVE ENDING MECHANISMS ------------------------------------------------
 
-  #access current mechanism list posted publically to DATIM
-  sql_view_url <- "https://www.datim.org/api/sqlViews/fgUtV6e9YIX/data.csv"
-  mech_official <- readr::read_csv(sql_view_url,
-                                   col_types = readr::cols(.default = "c"))
-
-  #rename variables to match MSD and remove mechid from mech name
-  ending_mechs <- mech_official %>%
-    dplyr::filter(agency == "USAID") %>%
-    dplyr::mutate(enddate = lubridate::ymd(enddate)) %>%
-    dplyr::select(mechanismid = code, enddate) %>%
-    dplyr::filter(lubridate::year(enddate) < 2020) %>%
-    dplyr::pull(mechanismid)
-
-  df_datim <- dplyr::filter(df_datim, !mechanismid %in% ending_mechs)
-
-  rm(sql_view_url, mech_official, ending_mechs)
+  # #access current mechanism list posted publically to DATIM
+  # sql_view_url <- "https://www.datim.org/api/sqlViews/fgUtV6e9YIX/data.csv"
+  # mech_official <- readr::read_csv(sql_view_url,
+  #                                  col_types = readr::cols(.default = "c"))
+  #
+  # #rename variables to match MSD and remove mechid from mech name
+  # ending_mechs <- mech_official %>%
+  #   dplyr::filter(agency == "USAID") %>%
+  #   dplyr::mutate(enddate = lubridate::ymd(enddate)) %>%
+  #   dplyr::select(mech_code = code, enddate) %>%
+  #   dplyr::filter(lubridate::year(enddate) < 2020) %>%
+  #   dplyr::pull(mech_code)
+  #
+  # df_datim <- dplyr::filter(df_datim, !mech_code %in% ending_mechs)
+  #
+  # rm(sql_view_url, mech_official, ending_mechs)
 
   # DUPICATE TARGETS --------------------------------------------------------
 
@@ -126,10 +149,14 @@ append_sources <- function(folderpath_hfr,
   # APPEND HFR AND TARGETS --------------------------------------------------
 
   df_hfr <- df_hfr %>%
-    dplyr::mutate(date = lubridate::mdy(date),
+    dplyr::mutate(#date = lubridate::mdy(date),
+                  date = as.Date(date),
+                  fy = as.integer(fy),
+                  hfr_pd = as.integer(hfr_pd),
                   sex = ifelse(sex == "Unknown", "Unspecified", sex),
-                  operatingunit = ifelse(operatingunit == "DRC","Democratic Republic of the Congo", operatingunit)) %>%
-    assign_pds() %>%
+                  operatingunit = ifelse(operatingunit == "DRC","Democratic Republic of the Congo", operatingunit),
+                  operatingunit = ifelse(operatingunit == "Viet Nam","Vietnam", operatingunit)) %>%
+    #assign_pds() %>%
     dplyr::bind_rows(df_datim_rpt)
 
   #aggregate to reduce # of lines
@@ -149,11 +176,27 @@ append_sources <- function(folderpath_hfr,
     df_hfr <- dplyr::filter(df_hfr, date <= as.Date(max))
   }
 
-  # EXPORT ------------------------------------------------------------------
+
+# ARRANGE COLUMNS ---------------------------------------------------------
+
+  df_hfr <- dplyr::select(df_hfr,
+                          operatingunit, orgunit, orgunituid, date, fy, hfr_pd,
+                          mech_code, mech_name, primepartner, fundingagency, snu1,
+                          psnu, community, indicator, agecoarse, sex, otherdisaggregate,
+                          val, mer_results, mer_targets, targets_gap, weekly_targets,
+                          weekly_targets_gap, dplyr::everything())
+
+# EXPORT ------------------------------------------------------------------
 
 
   if(!is.null(folderpath_output))
-    pd <- paste0("PD",max(df_hfr$hfr_pd))
+
+    pd <- df_hfr %>%
+      dplyr::distinct(fy, hfr_pd) %>%
+      dplyr::mutate(pd = fy + (hfr_pd/100)) %>%
+      dplyr::pull(pd) %>%
+      max()
+
     readr::write_csv(df_hfr,
                      file.path(folderpath_output,
                                paste0("HFR_GLOBAL_",

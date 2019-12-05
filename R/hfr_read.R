40e747503671bee8febad938d4fb33f2568a055d
Aaron Chafetz <achafetz@usaid.gov>
2019-12-05 10:57:39 -0500

    add new function to read in output file correctly from csv

---
+++ R/hfr_read.R
@@ -0,0 +1,32 @@
+#' Read in HFR output file
+#'
+#' @param filepath filepath of an HFR output file
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'  path <- "~/data/HFR_2020.01_Global_output_20191204.1705.csv"
+#'  df <- hfr_read(path)
+#'  }
+
+hfr_read <- function(filepath){
+  #import
+    df <- vroom::vroom(filepath, delim = ",", col_types = c(.default = "c"))
+
+  #conver date to date format
+    df <- dplyr::mutate(df, date = lubridate::as_date(date))
+
+  #convert year and period to integer
+    df <- dplyr::mutate_at(df, dplyr::vars(fy, hfr_pd), as.integer)
+
+  #covert other numeric variables to double
+    df <- dplyr::mutate_at(df, dplyr::vars(dplyr::starts_with("val"),
+                                           dplyr::starts_with("mer"),
+                                           dplyr::starts_with("weekly"),
+                                           dplyr::starts_with("targets")), as.double)
+
+  #convert blanks to NAs
+    df <- dplyr::mutate_if(df, is.character, ~ dplyr::na_if(., "(NA|)"))
+}
+

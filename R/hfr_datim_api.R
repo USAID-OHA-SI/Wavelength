57a68139b1957699fcbc1ef31c6651d7f88dee0c
Aaron Chafetz <achafetz@usaid.gov>
2019-12-07 08:25:19 -0500

    update export

--- R/hfr_datim_api.R
+++ R/hfr_datim_api.R
@@ -376,15 +376,7 @@
       df_combo <- periodize_targets(df_combo, quarters_complete)
 
     #export
-      if(!is.null(folderpath_output)){
-
-        iso <- dplyr::filter(iso_map, countryname == ou_name) %>%
-          dplyr::pull(iso)
-
-        filename <- paste0("HFR_DATIM_FY19Q", quarters_complete, "_", iso, "_", format(Sys.Date(),"%Y%m%d"), ".txt")
-
-        readr::write_tsv(df_combo, file.path(folderpath_output, filename), na = "")
-      }
+      hfr_export(df_combo, folderpath_output, type = "DATIM", quarters_complete)
 
     invisible(df_combo)
 

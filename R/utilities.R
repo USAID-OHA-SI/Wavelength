9de920dffe684b1460dfea91fb9f199215d05a57
Aaron Chafetz <achafetz@usaid.gov>
2019-12-09 16:27:38 -0500

    add validations to run inline with import

--- R/utilities.R
+++ R/utilities.R
@@ -61,3 +61,123 @@
     var %in% names(df)
 
   }
+
+
+#' Determine whether meta tab exists
+#'
+#' @param filepath filepath to sumbitted template
+#'
+#' @export
+
+is_metatab <- function(filepath){
+
+  if(missing(filepath))
+    stop("No filepath provided.")
+
+  shts <- readxl::excel_sheets(filepath)
+  "meta" %in% shts
+}
+
+
+
+#' Extract Meta Data Information about Template
+#'
+#' @description Useful for pulling information about the template, whether
+#' It be the Operating Unit (OU), Period, template version, or type, eg wide or long.
+#'
+#' @param filepath filepath to sumbitted template
+#' @param meta_type type of meta data requesting: ou, period, version, type (default)
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' #identify whether template is long or wide
+#'   filepath <- "~/WeeklyData/Raw/KEN_Weekly.xlsx"
+#'   extract_meta(filepath, meta_type = "type")
+#' #identify period
+#'   extract_meta(filepath, meta_type = "period")
+#' #identify OU
+#'   extract_meta(filepath, meta_type = "ou") }
+
+extract_meta <- function(filepath, meta_type = "type"){
+
+  if(is_metatab(filepath)){
+    metatable <- readxl::read_excel(filepath, range = "meta!B2:C5",
+                                    col_names = c("mtype", "mvalue"))
+
+    meta <- metatable %>%
+      dplyr::mutate(mtype =
+                      stringr::str_remove_all(mtype,
+                                              "Template|HFR FY and|, eg 2020.1|perating|nit| ")
+                    %>% tolower) %>%
+      dplyr::filter(mtype == meta_type) %>%
+      dplyr::pull()
+
+
+  } else {
+    meta <- NA
+  }
+
+  return(meta)
+}
+
+
+
+
+#' Count missing values
+#'
+#' @param df data frame
+#' @param var variable to count missing values
+#'
+#' @export
+
+count_missing <- function(df, var){
+
+  missing <- df %>%
+    dplyr::filter(is.na({{var}})) %>%
+    NROW()
+
+  missing_pct <- round(missing/NROW(df), 2)*100
+  missing_pct <- paste0("(",missing_pct, "%)")
+
+  count <- ifelse(missing > 0, crayon::red(missing, "out of", NROW(df), "rows", missing_pct), crayon::green("No"))
+  return(count)
+}
+
+
+#' Flag Missing Variables
+#'
+#' @param required list of required vars
+#' @param submitted list of vars pulled from submission
+#'
+  flag_missing <- function(required, submitted){
+
+    missing <- setdiff(required, submitted)
+    if(length(missing) > 0){
+      missing <- crayon::yellow(missing)
+    } else {
+      missing <- crayon::green("No")
+    }
+
+    return(missing)
+  }
+
+
+#' Flag Extra Variables
+#'
+#' @param required list of required vars
+#' @param submitted list of vars pulled from submission
+
+  flag_extra <- function(required, submitted){
+
+    extra <- setdiff(submitted, required)
+    if(length(extra > 0)){
+      extra <- crayon::red(extra)
+    } else {
+      extra <- crayon::green("No")
+    }
+
+    return(extra)
+  }
+
---
+++ R/validate_output.R
@@ -0,0 +1,159 @@
+#' Validation Checks
+#'
+#' @param df HFR data framed created by `hfr_process_template()`
+#'
+#' @export
+
+validate_output <- function(df){
+
+    check_output_cols(df)
+    check_dates(df)
+    check_orgunituids(df)
+    check_mechs(df)
+    check_inds(df)
+    check_disaggs(df)
+
+    }
+
+
+#' Validate columns for export
+#'
+#' @param df HFR data framed created by `hfr_process_template()`
+
+check_output_cols <-function(df){
+
+  #check headers
+    req_cols <- c("date", "fy", "hfr_pd", "orgunit",
+                  "orgunituid", "mech_code", "partner",
+                  "operatingunit", "psnu", "indicator",
+                  "agecoarse", "sex", "otherdisaggregate", "val")
+
+    submitted <- names(df)
+
+  #missing columns
+   missing <- flag_missing(req_cols, submitted)
+
+  #extra columns
+  extra <- flag_extra(req_cols, submitted)
+
+  #print validation
+    cat("\nAre there any missing columns for export?", missing,
+        "\nAre there any extra columns for export?", extra)
+}
+
+
+#' Validate dates
+#'
+#' @param df HFR data framed created by `hfr_process_template()`
+
+check_dates <-function(df){
+
+  #missing dates?
+    missing_dates <- count_missing(df, date)
+
+  #date range
+    pds <- length(unique(df$hfr_pd))
+
+    date_range <- df %>%
+      dplyr::distinct(date, fy, hfr_pd) %>%
+      dplyr::mutate(hfr_pd = hfr_pd/100,
+                    pd = paste0("[", fy + hfr_pd, "]")) %>%
+      tidyr::unite(date_pd, c("date", "pd"), sep = " ") %>%
+      dplyr::pull(date_pd) %>%
+      paste(collapse = ", ")
+    date_range <- ifelse(pds > 1, crayon::red(date_range), crayon::green(date_range))
+    pds <- ifelse(pds > 1, crayon::red("Yes"), crayon::green("No"))
+
+  #print validation
+  cat("\nAre there any missing dates?", missing_dates,
+      "\nDoes the submission cover multiple period?", pds,
+      "\nWhat dates does the submission cover?", date_range)
+}
+
+
+
+#' Validate orgunituids for export
+#'
+#' @param df HFR data framed created by `hfr_process_template()`
+
+check_orgunituids <-function(df){
+
+  #missing orgunituid?
+    missing_orgunituid <- count_missing(df, orgunituid)
+
+  #print validation
+    cat("\nAre there any missing orgunituids?", missing_orgunituid)
+}
+
+
+#' Validate mechanisms for export
+#'
+#' @param df HFR data framed created by `hfr_process_template()`
+
+check_mechs <-function(df){
+
+  #missing mechanisms?
+    missing_mechs<- count_missing(df, mech_code)
+
+  #mechanisms
+    mech_list <- unique(df$mech_code) %>% sort() %>% paste(collapse = ", ") %>% crayon::blue()
+
+  #print validation
+  cat("\nAre there any missing mech_codes?", missing_mechs,
+      "\nWhat mechanism are included?", mech_list)
+}
+
+
+#' Validate indicators for export
+#'
+#' @param df HFR data framed created by `hfr_process_template()`
+
+check_inds <-function(df){
+
+    missing_ind <- count_missing(df, indicator)
+
+  #indicators
+    req <- c("HTS_TST", "HTS_TST_POS", "PrEP_NEW",
+                 "TX_CURR", "TX_MMD", "TX_NEW", "VMMC_CIRC")
+    sumbitted <- unique(df$indicator)
+
+    missing <- flag_missing(req, sumbitted)
+    extra <- flag_extra(req, sumbitted)
+
+
+  #print validation
+  cat("\nAre there any unspecified indicators?", missing_ind,
+      "\nAre there any missing indicators?", missing,
+      "\nAre there any extra indicators?", extra)
+}
+
+
+#' Validate disaggs for export
+#'
+#' @param df HFR data framed created by `hfr_process_template()`
+
+check_disaggs <-function(df){
+
+  #age/sex
+    req <- c("Female <15", "Female 15+", "Male <15", "Male 15+")
+    submitted <- df %>%
+      dplyr::distinct(agecoarse, sex) %>%
+      tidyr::unite(agesex, c("sex", "agecoarse"), sep = " ") %>%
+      dplyr::pull(agesex)
+
+    missing <- flag_missing(req, submitted)
+    extra <- flag_extra(req, submitted)
+
+  #MMD months
+    req_otherdisagg <- c("<3 months", "3-5 months", "6 months or more")
+    sumbitted_otherdisagg <- unique(df$otherdisaggregate) %>% setdiff(NA)
+
+  #extra otherdisaggs
+    extra_otherdisagg <- flag_extra(req_otherdisagg, sumbitted_otherdisagg)
+
+  #print validation
+  cat( "\nAre there any missing age/sex disaggs?", missing,
+       "\nAre there any extra age/sex disaggs?", extra,
+       "\nAre there any extra other disaggs?", extra_otherdisagg)
+}
+

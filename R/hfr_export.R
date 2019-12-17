29230b228c14ffa2a0f2a4f329b3cb3a519fe5e4
Aaron Chafetz <achafetz@usaid.gov>
2019-12-17 15:30:32 -0500

    reoder export to better handle ctry output"\

--- R/hfr_export.R
+++ R/hfr_export.R
@@ -19,7 +19,64 @@ hfr_export <- function(df, folderpath_output = NULL,
 
   if(!is.null(folderpath_output)){
 
-    #get ISO code for file nameing
+    #export
+      cat("\nExport:\n")
+      if(by_mech == TRUE){
+        #by mechanism, compile file name  and export data
+        purrr::walk(.x = unique(df$mech_code),
+                    .f = ~ hfr_export_mech(df, .x, type, folderpath_output))
+      } else {
+        #get ISO code for file nameing
+          if(var_exists(df, "countryname")) {
+            ou <- unique(df$countryname)
+          } else if(var_exists(df, "operatingunit")) {
+            ou <- unique(df$operatingunit)
+            ou <- ifelse(stringr::str_detect(ou,"/"), stringr::str_remove(ou, "^.*/"), ou)
+          } else {
+            ou <- NULL
+          }
+
+          if(length(ou) == 1){
+            iso <- dplyr::filter(iso_map, operatingunit == ou) %>%
+              dplyr::pull(iso)
+          } else {
+            iso <- "GLOBAL"
+          }
+
+        #get date for file naming
+          date <- format(Sys.Date(),"%Y%m%d")
+
+        #get period for naming
+          if(var_exists(df, "hfr_pd")) {
+            pd <- median(df$fy) + median(df$hfr_pd)/100
+          } else if (!is.null(quarters_complete)) {
+            pd <- paste0("FY",curr_fy()-2000, "Q", quarters_complete)
+          } else {
+            pd <- paste0("FY",curr_fy()-2000)
+          }
+
+        #compile file name  and export data
+          filename <- paste("HFR", pd, iso, type, date, sep = "_") %>% paste0(".csv") %>% stringr::str_remove_all("_{2,}")
+          readr::write_csv(df, file.path(folderpath_output, filename), na = "")
+          cat(crayon::blue("         ",file.path(filename), "\n"))
+      }
+  }
+}
+
+
+#' Export csv files by mechanism
+#'
+#' @param df tructured High Frequency Data Frame
+#' @param mech mech_code
+#' @param type type type of data being saved, default = processed
+#' @param folderpath_output provide the full path to the folder for saving
+
+hfr_export_mech <- function(df, mech, type, folderpath_output){
+
+  #filter to mechanism
+    df_mech <- dplyr::filter(df, mech_code == mech)
+
+  #get ISO code for file nameing
     if(var_exists(df, "countryname")) {
       ou <- unique(df$countryname)
     } else if(var_exists(df, "operatingunit")) {
@@ -37,47 +94,18 @@ hfr_export <- function(df, folderpath_output = NULL,
     }
 
 
-    #get date for file naming
-      date <- format(Sys.Date(),"%Y%m%d")
+  #get date for file naming
+    date <- format(Sys.Date(),"%Y%m%d")
 
-    #get period for naming
-      if(var_exists(df, "hfr_pd")) {
-        pd <- median(df$fy) + median(df$hfr_pd)/100
-      } else if (!is.null(quarters_complete)) {
-        pd <- paste0("FY",curr_fy()-2000, "Q", quarters_complete)
-      } else {
-        pd <- paste0("FY",curr_fy()-2000)
-      }
+  #get period for naming
+    if(var_exists(df, "hfr_pd")) {
+      pd <- median(df$fy) + median(df$hfr_pd)/100
+    } else if (!is.null(quarters_complete)) {
+      pd <- paste0("FY",curr_fy()-2000, "Q", quarters_complete)
+    } else {
+      pd <- paste0("FY",curr_fy()-2000)
+    }
 
-    #export
-      cat("\nExport:\n")
-      if(by_mech == TRUE){
-        #by mechanism, compile file name  and export data
-        purrr::walk(.x = unique(df$mech_code),
-                    .f = ~ hfr_export_mech(df, pd, iso, .x, type, date, folderpath_output))
-      } else {
-        #compile file name  and export data
-        filename <- paste("HFR", pd, iso, type, date, sep = "_") %>% paste0(".csv") %>% stringr::str_remove_all("_{2,}")
-        readr::write_csv(df, file.path(folderpath_output, filename), na = "")
-        cat(crayon::blue("         ",file.path(filename), "\n"))
-      }
-  }
-}
-
-
-#' Export csv files by mechanism
-#'
-#' @param df tructured High Frequency Data Frame
-#' @param pd HFR period
-#' @param iso country ISO code
-#' @param mech mech_code
-#' @param type type type of data being saved, default = processed
-#' @param date today's date
-#' @param folderpath_output provide the full path to the folder for saving
-
-hfr_export_mech <- function(df, pd, iso, mech, type, date, folderpath_output){
-  #filter to mechanism
-    df_mech <- dplyr::filter(df, mech_code == mech)
   #compile file name
     filename <- paste("HFR", pd, iso, mech, type, date, sep = "_") %>% paste0(".csv") %>% stringr::str_remove_all("_{2,}")
   #export data

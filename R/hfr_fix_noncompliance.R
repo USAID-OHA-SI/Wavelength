eed7241070b7bb372b81095cd368c3e0f322a8aa
Aaron Chafetz <achafetz@usaid.gov>
2019-12-20 16:18:31 -0500

    reorganize non compliance fixes

--- R/hfr_fix_noncompliance.R
+++ R/hfr_fix_noncompliance.R
@@ -6,31 +6,20 @@
 
 hfr_fix_noncompliance <- function(df){
   df <- df %>%
-    #resolve issue with reporting for Thailand
-    dplyr::mutate(indicator = dplyr::case_when(indicator == "HTS_TST ALL" ~ "HTS_TST",
-                                               indicator == "HTS_TST POSITIVE" ~ "HTS_TST_POS",
-                                               TRUE ~ indicator),
-                  agecoarse = dplyr::case_when(agecoarse %in% c("15-19", "20-24", "30-34",
-                                                         "25-29", "35-39", "40-44", "45-49",
-                                                         "50+") ~ "15+",
-                                        TRUE ~ agecoarse),
-                  otherdisaggregate = dplyr::case_when(otherdisaggregate %in% c("MSM", "MSW", "TG",
-                                                                         "TG-SW", "Female Non-KP",
-                                                                         "FSW", "Male Non-KP") ~ as.character(NA),
-                                                TRUE ~ otherdisaggregate),
+    #resolve issue with reporting for:
+    dplyr::mutate(
+                  #CDI
+                  indicator = ifelse(indicator %in% c("TX_NEW_NewHIV", "TX_NEW_NEWHIV"), "TX_NEW", indicator),
                   #Laos
                   indicator = stringr::str_replace_all(indicator, "-", "_"),
                   agecoarse = ifelse(agecoarse == "Male 20-25", "15+", agecoarse),
                   #Lesotho
                   indicator = ifelse(indicator == "HTS_POS", "HTS_TST_POS", indicator),
-                  #Vietnam
-                  indicator = stringr::str_replace(indicator, "MMS", "MMD"),
-                  operatingunit = ifelse(operatingunit == "Viet Nam", "Vietnam", operatingunit),
-                  #South Sudan
-                  val = dplyr::na_if(val, "N/A"),
-                  val = dplyr::na_if(val, "M/A"),
-                  #CDI
-                  indicator = ifelse(indicator %in% c("TX_NEW_NewHIV", "TX_NEW_NEWHIV"), "TX_NEW", indicator),
+                  #Nigeria
+                  otherdisaggregate = dplyr::case_when(otherdisaggregate == "2Months" ~ "<3 months",
+                                                       otherdisaggregate %in% c("3Months", "4Months", "5Months") ~ "3-5 months",
+                                                       otherdisaggregate == "6Months" ~ "6 months or more",
+                                                       TRUE ~ otherdisaggregate),
                   #Moz
                   otherdisaggregate = ifelse(indicator == "TX_3MD", "3-5 months", otherdisaggregate),
                   indicator = ifelse(indicator == "TX_3MD", "TX_MMD", indicator),
@@ -41,7 +30,26 @@ hfr_fix_noncompliance <- function(df){
                   otherdisaggregate = dplyr::case_when(otherdisaggregate %in% c("1", "2") ~ "<3 months",
                                                        otherdisaggregate %in% c("3", "4", "5") ~ "3-5 months",
                                                        otherdisaggregate %in% c("6", "6+") ~ "6 months or more",
-                                                       TRUE ~ otherdisaggregate)
+                                                       TRUE ~ otherdisaggregate),
+                  #South Sudan
+                  val = dplyr::na_if(val, "N/A"),
+                  val = dplyr::na_if(val, "M/A"),
+                  #Thailand
+                  indicator = dplyr::case_when(indicator == "HTS_TST ALL" ~ "HTS_TST",
+                                               indicator == "HTS_TST POSITIVE" ~ "HTS_TST_POS",
+                                               TRUE ~ indicator),
+                  agecoarse = dplyr::case_when(agecoarse %in% c("15-19", "20-24", "30-34",
+                                                         "25-29", "35-39", "40-44", "45-49",
+                                                         "50+") ~ "15+",
+                                        TRUE ~ agecoarse),
+                  otherdisaggregate = dplyr::case_when(otherdisaggregate %in% c("MSM", "MSW", "TG",
+                                                                         "TG-SW", "Female Non-KP",
+                                                                         "FSW", "Male Non-KP") ~ as.character(NA),
+                                                TRUE ~ otherdisaggregate),
+
+                  #Vietnam
+                  indicator = stringr::str_replace(indicator, "MMS", "MMD"),
+                  operatingunit = ifelse(operatingunit == "Viet Nam", "Vietnam", operatingunit)
 
                   )
 

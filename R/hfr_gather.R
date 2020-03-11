de874645a1e0668e6a453f33e7446e1e34893583
Aaron Chafetz <achafetz@usaid.gov>
2020-03-11 14:15:55 -0400

    fix order for wide to match long w/ age/sex

--- R/hfr_gather.R
+++ R/hfr_gather.R
@@ -14,11 +14,13 @@ hfr_gather <- function(df){
         data_cols <- setdiff(names(df), template_cols_meta)
 
       #reshape from wide to long
-        df <- tidyr::gather(df, ind, val, data_cols)
+        df <- tidyr::gather(df, ind, val, all_of(data_cols))
 
       #seperate former col names into indicator & disaggregates
         df <- tidyr::separate(df, ind, c("indicator", "agecoarse", "sex", "otherdisaggregate"),
                               sep = "\\.", fill = "right")
+      #reorganize
+        df <- dplyr::select(df, date:indicator, sex, dplyr::everything())
     }
 
   return(df)

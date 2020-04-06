8abca780923f911b2429c978908db25fa5769843
Aaron Chafetz <achafetz@usaid.gov>
2020-04-06 16:32:05 -0400

    add wide limited cols to validate against

--- R/validate_import.R
+++ R/validate_import.R
@@ -9,6 +9,8 @@ validate_import <- function(df){
   #check columns
   if(var_exists(df, "val")){
     req_cols <- template_cols_long
+  } else if(var_exists(df, "hts_tst.NA.NA")){
+    req_cols <- template_cols_wide_lim
   } else {
     req_cols <- template_cols_wide
   }

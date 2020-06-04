658ac58be0b232770c251ae41a21ff3c6daa23c8
Aaron Chafetz <achafetz@usaid.gov>
2020-06-04 08:57:31 -0400

    fix major aggregate bug

--- data-raw/completeness_report.R
+++ data-raw/completeness_report.R
@@ -2,7 +2,7 @@
 ## AUTHOR:   A.CHAFETZ | USAID
 ## PURPOSE:  OU completeness report
 ## DATE:     2019-10-03
-## UPDATED:  2020-06-02
+## UPDATED:  2020-06-04
 
 
 # DEPENDENCIES ------------------------------------------------------------
@@ -23,16 +23,6 @@
     path <- "out/joint/HFR_2020.07_Tableau_20200518_adj.csv"
     # path_new <- "out/joint/HFR_GLOBAL_2020.02_output_20200107.0838.csv"
 
-  #numeric variables to convert from string
-    vars_num <- c("mer_results", "mer_targets", "targets_gap", "weekly_targets", "weekly_targets_gap", "val")
-
-  #list of OUs to review
-    ou_lst <- c("Botswana", "Burundi", "Cameroon", "Cote d'Ivoire",
-                "Democratic Republic of the Congo", "Dominican Republic",
-                "Eswatini", "Ethiopia", "Haiti", "Kenya", "Lesotho",
-                "Malawi", "Mozambique", "Namibia", "Nigeria", "South Africa",
-                "South Sudan", "Tanzania", "Uganda", "Ukraine", "Vietnam",
-                "Zambia", "Zimbabwe")
   #red
     flag_red <- brewer.pal(5, "OrRd")[5]
 
@@ -48,23 +38,15 @@
 
 # MUNGE -------------------------------------------------------------------
 
-  #adjust numeric variables and fix issue with lines w/ HTS_TST POS
-    # df_glob <- df_glob %>%
-    #   dplyr::mutate_at(dplyr::vars(vars_num), as.numeric) %>%
-    #   dplyr::mutate_at(dplyr::vars(fy, hfr_pd), as.integer)
-
   #adjust period
-    df_glob <- dplyr::mutate(df_glob, hfr_pd = fy + (hfr_pd/100))
+    df_glob <- mutate(df_glob, hfr_pd = fy + (hfr_pd/100))
 
   #add iso codes before adjusting OU name
     df_glob <- left_join(df_glob, iso_map)
 
-  #filter out PD 8 (not officially collected) & restrict to OU's in list; shorten to DRC
+  #rename OUs
     df_glob <-  df_glob %>%
-      # dplyr::filter(hfr_pd > 2019.08,
-      #               operatingunit %in% ou_lst
-      #               ) %>%
-      dplyr::filter(operatingunit != "\\N") %>%
+      filter(operatingunit != "\\N") %>%
       dplyr::mutate(operatingunit = recode(operatingunit,
                                            "Democratic Republic of the Congo" ="DRC",
                                            "Dominican Republic" = "DR",
@@ -72,99 +54,92 @@
                                            "Western Hemisphere Region" = "WHR"
                                            ))
 
-  #create denom for TX_MMD
-    df_glob_mmd <- df_glob %>%
-      dplyr::filter(indicator == "TX_CURR") %>%
-      dplyr::group_by(operatingunit, iso, snu1, psnu, orgunit, mech_code, hfr_pd, indicator) %>% #orgunituid
-      dplyr::summarise_at(dplyr::vars(mer_targets), sum, na.rm = TRUE) %>%
-      dplyr::ungroup() %>%
-      dplyr::mutate(indicator = "TX_MMD")
+# AGGREGATE ---------------------------------------------------------------
 
+  #aggregate sites total (agg age/sex or months) to preserve distinct sites
+    df_glob_site_agg <- df_glob %>%
+      group_by(operatingunit, iso, orgunituid, mech_code, hfr_pd, date, indicator) %>%
+      summarise_at(vars(mer_targets, val), sum, na.rm = TRUE) %>%
+      ungroup()
+
+  #aggregate pd total for non-cumulative indicators
+    df_glob_pd_agg <- df_glob_site_agg %>%
+      filter(!indicator %in% c("TX_CURR", "TX_MMD")) %>%
+      mutate(target_sitecnt = ifelse(mer_targets > 0, 1, 0),
+             hfr_sitecnt = ifelse(val > 0, 1, 0)) %>%
+      group_by(operatingunit, iso, mech_code, indicator, hfr_pd) %>%
+      summarise_at(vars(mer_targets, val, target_sitecnt, hfr_sitecnt), sum, na.rm = TRUE) %>%
+      ungroup()
+
+  #agg max site value for cumulative indicators
+    df_glob_site_txcurr_agg <- df_glob_site_agg %>%
+      filter(indicator %in% c("TX_CURR", "TX_MMD")) %>%
+      group_by(operatingunit, iso, orgunituid, mech_code, hfr_pd, indicator) %>%
+      summarise_at(vars(mer_targets, val), max, na.rm = TRUE) %>%
+      ungroup()
+
+  #create TX_MMD pseudo target target
+    df_glob_site_mmdtarg <- df_glob_site_txcurr_agg %>%
+      filter(indicator == "TX_CURR") %>%
+      select(-val) %>%
+      mutate(indicator == "TX_MMD")
+
+  #append mmd targets on, create site counts and agg to pd
+    df_glob_site_txcurr_agg <- df_glob_site_txcurr_agg %>%
+      bind_rows(df_glob_site_mmdtarg) %>%
+      mutate(target_sitecnt = ifelse(mer_targets > 0, 1, 0),
+             hfr_sitecnt = ifelse(val > 0, 1, 0)) %>%
+      group_by(operatingunit, iso, mech_code, indicator, hfr_pd) %>%
+      summarise_at(vars(mer_targets, val, target_sitecnt, hfr_sitecnt), sum, na.rm = TRUE) %>%
+      ungroup()
+
+  #combine cumulative + non cumulative
+    df_glob_agg <- bind_rows(df_glob_pd_agg, df_glob_site_txcurr_agg)
+
+    rm(df_glob_pd_agg, df_glob_site_agg, df_glob_site_mmdtarg, df_glob_site_txcurr_agg)
 
 # SUMMARIZE COMPLETENESS --------------------------------------------------
 
-  #append MMD "target" sites
-    df_glob_sites <- dplyr::bind_rows(df_glob, df_glob_mmd)
-
-  #distinct set of sites w/ their MER targets and HFR values
-    df_glob_sites <- df_glob_sites %>%
-      dplyr::group_by(operatingunit, iso, snu1, psnu, orgunit, mech_code, hfr_pd, indicator) %>% #orgunituid
-      dplyr::summarise_at(dplyr::vars(mer_targets, val), sum, na.rm = TRUE) %>%
-      dplyr::ungroup()
-
-  #create site count for targets & HFR reporting
-    df_glob_sites_comp <- df_glob_sites %>%
-      dplyr::mutate(target_sitecnt = ifelse(mer_targets > 0, 1, 0),
-                    hfr_sitecnt = ifelse(val > 0, 1, 0)) %>%
-      dplyr::group_by(operatingunit, iso, mech_code, indicator, hfr_pd) %>%
-      dplyr::summarise_at(dplyr::vars(mer_targets, val, target_sitecnt, hfr_sitecnt), sum, na.rm = TRUE) %>%
-      dplyr::ungroup()
-
-  #record the distinct site count for each OUxIndicator
-    df_glob_sites_n <- df_glob_sites_comp %>%
-      dplyr::group_by(operatingunit, indicator) %>%
-      dplyr::summarise_at(dplyr::vars(target_sitecnt), max, na.rm = TRUE) %>%
-      dplyr::ungroup() %>%
-      dplyr::mutate(indicator = paste0(indicator, "_n")) %>%
-      dplyr::rename(completeness = target_sitecnt)
-
-  #create completeness metric
-    df_glob_sites_comp <- df_glob_sites_comp %>%
-      dplyr::mutate(completeness = hfr_sitecnt/target_sitecnt)
-
-  #clean for export to Excel (clipr - value to clipboard)
-    df_glob_sites_comp %>%
-      dplyr::filter(hfr_pd >= 2019.12) %>%
-      dplyr::select(-c(mer_targets, val, target_sitecnt, hfr_sitecnt)) %>%
-      dplyr::arrange(operatingunit, indicator, hfr_pd) %>%
-      tidyr::unite("indicator", c("indicator", "hfr_pd"), sep = "_pd") %>%
-      dplyr::bind_rows(df_glob_sites_n) %>%
-      tidyr::spread(indicator, completeness) %>%
-      dplyr::mutate_at(dplyr::vars(dplyr::ends_with("_n")), ~ dplyr::na_if(., 0)) %>%
-      dplyr::select(operatingunit, dplyr::starts_with("HTS_TST"), dplyr::starts_with("TX_NEW"),
-                    dplyr::starts_with("VMMC_CIRC"), dplyr::starts_with("PrEP_NEW"),
-                    dplyr::starts_with("TX_CURR"), dplyr::starts_with("TX_MMD"))
-
 
   #heatmap by country x indicator
-    df_glob_sites_comp %>%
-      dplyr::mutate(tx_curr_tgts = case_when(indicator == "TX_CURR" ~ mer_targets),
-                    operatingunit = fct_reorder(operatingunit, tx_curr_tgts, sum, na.rm = TRUE),
-                    hfr_pd = str_sub(hfr_pd, -2),
-                    indicator = factor(indicator, c("TX_CURR", "TX_MMD",
-                                                    "HTS_TST", "HTS_TST_POS",
-                                                    "TX_NEW", "VMMC_CIRC", "PrEP_NEW"
-                                                    )),
-                    completeness = ifelse(is.infinite(completeness), NA, completeness),
-                    comp_bin = case_when(is.na(completeness) ~ NA_character_,
-                                         completeness == 0 ~ "1",
-                                         completeness <= .25 ~ "2",
-                                         completeness <= .5 ~ "3",
-                                         completeness <= .75 ~ "4",
-                                         TRUE ~ "5"
-                                         )) %>%
-      ggplot(aes(hfr_pd, operatingunit, fill = comp_bin)) +
-      geom_tile(color = "white") +
-      geom_text(aes(label = percent(completeness,1), color = completeness > .75),
-                family = "Source Sans Pro", size = 2.5) +
-      facet_grid(~ indicator) +
-      scale_fill_brewer(palette = "OrRd", direction = -1) +
-      scale_color_manual(values = c("gray10", "gray50")) +
-      labs(x = NULL, y = NULL,
-           title = "LARGE REPORTING GAPS STILL EXISTS FOR KEY INDICATORS LIKE TX_CURR AND MMD",
-           subtitle = "mechanism x site completeness of reporting by period",
-           caption = "data as of HFR 2020.07 [2020-05-18]") +
-      si_style_nolines() +
-      #coord_fixed(ratio = 1) +
-      theme(legend.position = "none",
-            #panel.spacing = unit(.5, "lines"),
-            strip.text = element_text(face = "bold"))
+    # df_glob_agg %>%
+    #   dplyr::mutate(tx_curr_tgts = case_when(indicator == "TX_CURR" ~ mer_targets),
+    #                 operatingunit = fct_reorder(operatingunit, tx_curr_tgts, sum, na.rm = TRUE),
+    #                 hfr_pd = str_sub(hfr_pd, -2),
+    #                 indicator = factor(indicator, c("TX_CURR", "TX_MMD",
+    #                                                 "HTS_TST", "HTS_TST_POS",
+    #                                                 "TX_NEW", "VMMC_CIRC", "PrEP_NEW"
+    #                                                 )),
+    #                 completeness = ifelse(is.infinite(completeness), NA, completeness),
+    #                 comp_bin = case_when(is.na(completeness) ~ NA_character_,
+    #                                      completeness == 0 ~ "1",
+    #                                      completeness <= .25 ~ "2",
+    #                                      completeness <= .5 ~ "3",
+    #                                      completeness <= .75 ~ "4",
+    #                                      TRUE ~ "5"
+    #                                      )) %>%
+    #   ggplot(aes(hfr_pd, operatingunit, fill = comp_bin)) +
+    #   geom_tile(color = "white") +
+    #   geom_text(aes(label = percent(completeness,1), color = completeness > .75),
+    #             family = "Source Sans Pro", size = 2.5) +
+    #   facet_grid(~ indicator) +
+    #   scale_fill_brewer(palette = "OrRd", direction = -1) +
+    #   scale_color_manual(values = c("gray10", "gray50")) +
+    #   labs(x = NULL, y = NULL,
+    #        title = "LARGE REPORTING GAPS STILL EXISTS FOR KEY INDICATORS LIKE TX_CURR AND MMD",
+    #        subtitle = "mechanism x site completeness of reporting by period",
+    #        caption = "data as of HFR 2020.07 [2020-05-18]") +
+    #   si_style_nolines() +
+    #   #coord_fixed(ratio = 1) +
+    #   theme(legend.position = "none",
+    #         #panel.spacing = unit(.5, "lines"),
+    #         strip.text = element_text(face = "bold"))
+    #
+    # ggsave("out/Completeness050607.png", dpi = 330, height = 5.625, width = 10)
 
-    ggsave("out/Completeness050607.png", dpi = 330, height = 5.625, width = 10)
 
 
-
-   df_viz <- df_glob_sites_comp %>%
+   df_viz <- df_glob_agg %>%
       filter(indicator == "TX_CURR") %>%
       dplyr::mutate(tx_curr_tgts = case_when(indicator == "TX_CURR" ~ mer_targets),
                     completeness = ifelse(is.infinite(completeness), NA, completeness))
@@ -190,23 +165,6 @@
      filter(!is.na(comp_prior_bin))
 
    df_viz <- rename_official(df_viz)
-
-
-   #plot completened by PD
-   # df_viz %>%
-   #   filter(indicator == "TX_CURR") %>%
-   #   mutate(completeness = ifelse(completeness > 1, 1, completeness)) %>%
-   #   ggplot(aes(tx_curr_tgts, completeness, fill = completeness)) +
-   #   geom_point(size = 4, alpha = .5, shape = 21, stroke = 1, color = "gray70") +
-   #   facet_grid(indicator ~ hfr_pd) +
-   #   scale_x_log10(label = comma) +
-   #   scale_y_continuous(label = percent) +
-   #   scale_fill_distiller(palette = "OrRd", direction = -1, na.value = "grey50") +
-   #   labs(x = "FY20 TX_CURR MER TARGETS", y = "HFR Site Reporting Completeness",
-   #        caption = "HFR Data [2020-05-20]") +
-   #   si_style() +
-   #   theme(legend.position = "none",
-   #         strip.text = element_text(face = "bold"))
 
 
    #partner names for aligning parnters
@@ -314,7 +272,7 @@
      # geom_text_repel(aes(label = lab), family = "Source Sans Pro", size = 3,
      #   color = "gray40", force = 30) +
      scale_x_log10(label = comma) +
-     scale_y_continuous(label = percent_format(1), limits = c(-0.03, .2)) +
+     scale_y_continuous(label = percent_format(1), limits = c(-0.03, .23)) +
      scale_fill_identity() +
      expand_limits(y = -.05) +
      labs(x = NULL, y = NULL) +

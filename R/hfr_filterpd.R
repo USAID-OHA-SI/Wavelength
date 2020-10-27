1d821619eee15896215b2e360667fb30904675a2
Aaron Chafetz <achafetz@usaid.gov>
2020-10-27 14:04:19 -0400

    change hfr_filter_pd fy default to NULL, use curr_fy if NULL

--- R/hfr_filterpd.R
+++ R/hfr_filterpd.R
@@ -2,18 +2,20 @@
 #'
 #' @param df HFR data frame imported via `hfr_import()`
 #' @param hfr_pd_sel HFR reporting period, 1-13, no filter when NULL, default = NULL
-#' @param hfr_fy_sel fiscal year, default = 2020
+#' @param hfr_fy_sel fiscal year, default = NULL
 #'
 #' @export
 
-hfr_filter_pd <- function(df, hfr_pd_sel = NULL, hfr_fy_sel = 2020){
+hfr_filter_pd <- function(df, hfr_pd_sel = NULL, hfr_fy_sel = NULL){
 
+  if(is.null(hfr_fy_sel))
+   hfr_fy_sel <- curr_fy
 
   if(!is.numeric(hfr_fy_sel))
-    stop("Enter hfr_fy_sel as an integer, eg 2020")
+    stop("Enter hfr_fy_sel as an integer, eg 2021")
 
   if(!is.null(hfr_pd_sel) && !is.numeric(hfr_pd_sel))
-    stop("Enter hfr_pd_sel as an integer between 1 and 13")
+    stop("Enter hfr_pd_sel as an integer between 1 and 12")
 
   if(!is.null(hfr_pd_sel))
     df <- df %>%
